e65b6c298fa5a9c3d25032cdbcee3effa256674c
Daniel Machado <danielmcis@gmail.com>
2022-04-05 00:30:37 +0000

    save commit

---
+++ handlers/files.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"net/http"
+	"path/filepath"
+
+	"github.com/danielmachado86/file_server/files"
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/go-hclog"
+)
+
+type Files struct {
+	log   hclog.Logger
+	store files.Storage
+}
+
+func NewFiles(s files.Storage, l hclog.Logger) *Files {
+	return &Files{store: s, log: l}
+}
+
+func (f *Files) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fn := vars["filename"]
+
+	f.log.Info("Handle POST", "id", id, "filename", fn)
+
+	f.saveFile(id, fn, rw, r)
+}
+
+func (f *Files) saveFile(id, path string, rw http.ResponseWriter, r *http.Request) {
+	f.log.Info("Save file for product", "id", id, "path", path)
+
+	fp := filepath.Join(id, path)
+	err := f.store.Save(fp, r.Body)
+	if err != nil {
+		f.log.Error("Unable to save file", "error", err)
+		http.Error(rw, "Unable to save file", http.StatusInternalServerError)
+	}
+}
